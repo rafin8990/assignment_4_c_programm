44c0830b64795c94e55c0bc46e845a893af35abc
rafin8990 <rafin8990@gmail.com>
2024-11-15 19:31:06 +0600

    practice added

--- practice.c
+++ practice.c
@@ -691,8 +691,7 @@ int main() {
 }
  */
 
-
-//problem 4 solve : same same but different
+// problem 4 solve : same same but different
 /* #include <stdio.h>
 #include <string.h>
 
@@ -749,7 +748,7 @@ int main()
 }
  */
 
-//problem 5 solve : find the missing number:
+// problem 5 solve : find the missing number:
 
 /* #include <stdio.h>
 
@@ -763,23 +762,105 @@ int main() {
 
     scanf("%lld", &T);
     for (long long i = 0; i < T; i++) {
-     
+
         scanf("%lld %lld %lld %lld", &Total, &A, &B, &C);
 
         long long multiplication= A * B * C;
 
         if ((Total % multiplication )== 0) {
-            missing_number = Total / multiplication;  
+            missing_number = Total / multiplication;
             printf("%lld\n", missing_number);
         } else {
-            printf("-1\n"); 
+            printf("-1\n");
         }
     }
-    
+
     return 0;
 }
 
  */
 
+/*
+Alisa and you have gone out for shopping, and Alisa wants to buy a new pair of shoes for Eid. She has enough money to buy anything. However, Alisa will only buy shoes if you also buy a pair. And you will buy a pair of shoes if you can buy a Punjabi. That means, everything is depending on the Punjabi.
+
+You have decided that you will buy a Punjabi only if you have more than 1000 Taka. After purchasing the Punjabi the amount of your money will be reduced by 1000. Suppose you have 1600 taka with you, after buying the Punjabi you will have 600 taka left with you.
+
+Then you will only buy shoes if you have 500 Taka or more left with you. That means, if you can't buy your Punjabi you can't buy shoes.
+
+Now if I inform you the amount N Taka that your mother will give you, can you tell me what will happen next?
+
+If you buy a punjabi print "I will buy Punjabi".
+
+If you buy a pair of shoes print "I will buy new shoes"
+
+If Alisa buy a pair of shoes print "Alisa will buy new shoes"
+
+If no one can buy anything print "Bad luck!"
+
+Note: Don't forget to print new line after every line you print.
+
+Input Format
+
+Input will contain a non-negative integer N.
+Constraints
+
+1 <= N <= 2^31
+Output Format
+
+Output the events that will happen as asked in the question.
+ */
+/* #include <stdio.h>
+
+int main() {
+    long long int N;
+    scanf("%lld", &N);
 
 
+    if (N > 1000) {
+        printf("I will buy Punjabi\n");
+
+        long long int remaining = N - 1000;
+
+        if (remaining >= 500) {
+            printf("I will buy new shoes\n");
+            printf("Alisa will buy new shoes\n");
+        }
+    } else {
+        printf("Bad luck!\n");
+    }
+
+    return 0;
+}
+ */
+/* 
+#include <stdint.h>
+
+int main()
+{
+
+   printf("Hello, world! I am learning C programming language.^_^\n");
+    printf("Programming is fun and challenging. /\\/\\/\\\n");  
+    printf("I want to give my 100%% dedication to learn!"); 
+    printf(" I will succeed one day.\n");
+    
+    return 0;
+}
+ */
+/* 
+
+// divisible by 3 and 7
+#include <stdio.h>
+
+int main() {
+ 
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        if (i % 21 == 0) {  
+            printf("%d\n", i);  
+        }
+    }
+    return 0;
+}
+ */
